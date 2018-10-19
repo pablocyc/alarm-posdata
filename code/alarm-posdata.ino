465d7150485fd9fd8ba7f974414c7d0cfa860337
Pablocyc <pablocyc83@gmail.com>
2018-10-18 21:53:51 -0400

    inicializando proyecto

---
+++ code/alarm-posdata.ino
@@ -0,0 +1,11 @@
+/*******************************************************************
+ *  Programa para el control de un sistema de alarma.              *
+ *  El control se lo realiza por un teclado KR602.                 *
+ *  Tiene adicional un control de timbre accionado por un NV5      *
+ *                      chatsID (numeros) autorizados              *
+ *                                                                 *
+ *    Author:   Pablo Cardozo                                      *
+ *    Company:  Electric Eye                                       *
+ *    Position: Frontend Developer                                 *
+ *                                                                 *
+ *******************************************************************/
